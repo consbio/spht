d8484c54d1f423289875063781163baa83f64c9f
Nik Molnar <nik.molnar@consbio.org>
2018-04-26 10:34:43 -0700

    Fix loading webpack vendor file

--- spht_project/settings.py
+++ spht_project/settings.py
@@ -39,6 +39,8 @@ SECRET_KEY = CONFIG.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = CONFIG.get('debug', True)
 
+INTERNAL_IPS = ['127.0.0.1']
+
 ALLOWED_HOSTS = CONFIG.get('allowed_hosts', [])
 
 
